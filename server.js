95d5efa112fd19f70ab4c1a42535d92ab9709628
Ladipo Layode <ladipol@gmail.com>
2022-01-17 12:33:09 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
-const express = require('express');
+import express from 'express';
+//const express = require("express");
 const app = express();
+import bodyParser from 'body-parser';
+//const bodyParser = require("body-parser");
 
-const mockUserData=[
-  {name:'Ladipo'},
-  {name: 'Titilayo'}
-]
-app.get('/users', function(req, res){
+app.use(bodyParser.json());
+
+const mockUserData = [{ name: "Ladipo" }, { name: "Titilayo" }];
+app.get("/users", function (req, res) {
   res.json({
     success: true,
-    message: 'successfully got users. Nice!',
-    users: mockUserData
-  })
-})
-app.get('/users/:id',function(req,res){
-  console.log(req.params.id)
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+// colons are used as variables that be viewed in the params
+app.get("/users/:id", function (req, res) {
+  console.log(req.params.id);
   res.json({
     success: true,
-    message: 'got one user',
-    user: req.params.id
-  })
-})
+    message: "got one user",
+    user: req.params.id,
+  });
+});
 
-app.listen(8000,function(){
-  console.log("server is running")
-})
+app.post("/login", function (req, res) {
+  // Typically passwords are encrypted using something like bcrypt before sending to database
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "Oladipupo";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    // In practice, use JSON web token sign method here to make an encrypted token
+    re.json({
+      success: true,
+      message: "password and username match!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
+});
+app.listen(8000, function () {
+  console.log("server is running");
+});
